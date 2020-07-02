97433a5b765a22444e69ead620bb481f2d1b72ae
gileslin1213 <33174637+gileslin1213@users.noreply.github.com>
2020-07-02 15:48:15 +0800

    Update all.js

--- js/all.js
+++ js/all.js
@@ -51,7 +51,7 @@ function GetStream() {
   $.ajax({
     type: "GET",
     url: "https://api.twitch.tv/helix/streams",
-    headers: { 'Client-ID': 'nykxj742gwdpflmj32jzltnv4u4ctt','Authorization':'Bearer sv5gxbinv03jneix3h7ki0ftri1ppv' },
+    headers: { 'Client-ID': 'nykxj742gwdpflmj32jzltnv4u4ctt','Authorization':'Bearer 26fmakhz5qv8m3ly4ruzb3q9w25ldi' },
     data: {
       "game_id": game,
       "language": language,
